cc2c09bac42d2a7fc17762c48ab77531c4509e3f
Tobias Aagaard <Tobias909@live.dk>
2023-12-18 10:54:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,20 @@ ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 200, 300)
 
 ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(100, 100, 300, 300)
+ctx.fillRect(100, 100, 300, 350)
 
-ctx.fillStyle = 'green';
+ctx.fillStyle = 'rgb(50, 300, 60)';
 ctx.fillRect(200, 200, 300, 400)
+
+
+
+ctx.fillStyle = 'yellow';
+ctx.beginPath();
+ctx.ellipse(700, 400, 200, 200, Math.PI / 45, 3, 2 * Math.PI);
+ctx.fill();
+
+ctx.fillStyle = 'pink';
+ctx.beginPath();
+ctx.ellipse(700, 400, 200, 200, Math.PI / 1, 3, 2 * Math.PI);
+ctx.fill();
+

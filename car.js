bba52c21b84854a8e648af1f58a289b2096d885e
Tobias Aagaard <Tobias909@live.dk>
2023-12-18 10:09:36 +0100

    Canvas
    Starting canvas with minimal styleing

---
+++ car.js
@@ -0,0 +1,24 @@
+
+class car {
+    constructor (brand, model, year, condition) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.condition = condition;
+
+
+        
+
+    }
+
+}
+
+
+myCar = [];
+
+myCar.push = new car('Volvo', 'v18', 1998, 'god')
+myCar.push = new car('Porche', '911', 2023, 'ny')
+
+
+console.log(myCar)
+

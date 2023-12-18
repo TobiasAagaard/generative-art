f5676731b0ed15a4270acc22c71b00935c3ab00c
Tobias Aagaard <Tobias909@live.dk>
2023-12-18 09:49:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ class car {
         this.year = year;
         this.condition = condition;
 
+
+        getInfo(){
+            return`jeg er en ${brand} og en ${model}. jeg er fra år ${year}, og jeg er i ${condition} stand`;
+        }
+
     }
 
 }
 
+
+myCar = [];
 
 const car1 = new car('Volvo', 'v18', 1998, 'god')
 

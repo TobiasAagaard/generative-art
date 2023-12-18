482d0be758a722515344c2910dff6b386b7ecc4b
Tobias Aagaard <Tobias909@live.dk>
2023-12-18 10:28:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ console.log(ctx)
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 200, 300)
 
-ctx.fillStyle = 'blue';
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
 ctx.fillRect(100, 100, 300, 300)
 
 ctx.fillStyle = 'green';
-ctx.fillRect(200, 200, 300, 600)
+ctx.fillRect(200, 200, 300, 400)

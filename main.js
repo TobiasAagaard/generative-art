97e48e45666171e4f7c56b436678dccb836042e5
Tobias Aagaard <Tobias909@live.dk>
2023-12-18 09:37:18 +0100

    Første car object

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 
-class mytest {
-    constructor (msg) {
-        console.log('Hello there ' + msg)
+class car {
+    constructor (brand, model, year, condition) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.condition = condition;
+
     }
 
 }
 
 
-const myObject = new mytest('Hello you too')
+const car1 = new car('Volvo', 'v18', 1998, 'god')
+
+
+console.log(car1)
+
